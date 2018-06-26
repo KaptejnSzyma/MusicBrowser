ad7c0dc4e33782352c4b867bec69293b6c24809b
Szymek <szymon.rzadca@gmail.com>
2018-06-26 11:36:33 +0200

    creating a skeleton of the window, rows and columns

--- Jukebox.py
+++ Jukebox.py
@@ -6,3 +6,17 @@ except ImportError:
 
 conn = sqlite3.connect('music.sqlite')
 
+mainWindow = tkinter.Tk()
+mainWindow.title("Music DB Browser")
+mainWindow.geometry("1024x768")
+
+mainWindow.columnconfigure(0, weight=2)
+mainWindow.columnconfigure(1, weight=2)
+mainWindow.columnconfigure(2, weight=2)
+mainWindow.columnconfigure(3, weight=1) # spacer column
+
+mainWindow.rowconfigure(0, weight=1)
+mainWindow.rowconfigure(1, weight=5)
+mainWindow.rowconfigure(2, weight=5)
+mainWindow.rowconfigure(3, weight=1)
+
