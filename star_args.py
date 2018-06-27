e53540793d8e5f47cc1cba9aa87ba7487fba8472
Szymek <szymon.rzadca@gmail.com>
2018-06-27 15:50:47 +0200

    messing with kwargs

--- star_args.py
+++ star_args.py
@@ -9,9 +9,10 @@
 def print_backwards(*args, **kwargs):
     end_character = kwargs.pop('end', '\n')
     sep_character = kwargs.pop('sep', ' ')
-    for word in args[::-1]:
+    for word in args[:0:-1]: # change the range
         print(word[::-1], end=sep_character, **kwargs)
-    print(end=end_character)
+    print(args[0][::-1], end=end_character, **kwargs)  # and print the word separately
+    # print(end=end_character) # which means we don't need this line
 
 
 with open("backwards.txt", 'w') as backwards:
@@ -20,4 +21,4 @@ with open("backwards.txt", 'w') as backwards:
 print()
 print("hello", "planet", "earth", "take", "me", "to", "your", "leader", end='', sep='\n**\n')
 print_backwards("hello", "planet", "earth", "take", "me", "to", "your", "leader", end='', sep='\n**\n')
-print('*' * 10)
+print('=' * 10)
