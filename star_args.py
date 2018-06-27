d3b8cee6029e6ff038f28a07cab188b3a65603f5
Szymek <szymon.rzadca@gmail.com>
2018-06-27 15:27:31 +0200

    defining print backwards function

--- star_args.py
+++ star_args.py
@@ -1,24 +1,5 @@
-def average(*args):
-    print(type(args))
-    print("args is {}".format(args))
-    print("*args is: ", *args)
-    mean = 0
-    for arg in args:
-        mean += arg
-    return mean / len(args)
+def print_backwards(*args):
+    for word in args[::-1]:
+        print(word[::-1], end=' ')
 
-
-def build_tuple(*args):
-    return args
-
-
-print(average(1, 2, 3, 4))
-
-
-message_tuple = build_tuple("hello", "planet", "earth", "take")
-print(type(message_tuple))
-print(message_tuple)
-
-number_tuple = build_tuple(1, 2, 3, 4, 5, 6)
-print(type(number_tuple))
-print(number_tuple)
+print_backwards("hello", "planet", "earth", "take", "me", "to", "your", "leader")
