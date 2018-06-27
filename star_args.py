a6db36925f96a71f6ae4a2a942191c61628dfdd1
Szymek <szymon.rzadca@gmail.com>
2018-06-27 15:17:47 +0200

    creating build tuple function as a challenge

--- star_args.py
+++ star_args.py
@@ -7,4 +7,14 @@ def average(*args):
         mean += arg
     return mean / len(args)
 
+
+def build_tuple(*args):
+    return tuple(args)
+
+
 print(average(1, 2, 3, 4))
+
+
+message_tuple = build_tuple("hello", "planet", "earth", "take")
+print(type(message_tuple))
+print(message_tuple)
